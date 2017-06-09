c5416fec482902a1a0baa444ee8c7f0be48260fd
pcebrian <paucebr@gmail.com>
2017-06-09 22:52:19 +0200

    update run file

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-CUDA_VISIBLE_DEVICES=$1 python train.py -c config/camvid_adversarial_semseg.py -e $2 -s /home/pcebrian/experiments -l /home/pcebrian/experiments
+# execute with ./run free_gpu_device experiment_name
+# check free gpu devices with nvidia-smi
+# dont forget to change configuration file and user paths to save experiments
+
+CUDA_VISIBLE_DEVICES=$1 python train.py -c config/camvid_adversarial_semseg.py -e $2 -s /home/user/experiments -l /home/user/experiments
